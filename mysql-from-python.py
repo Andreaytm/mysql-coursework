25e2899ded074ac6214b46e0b3021ebf9dfb21c8
Andrea Ma <andrea.ma@hotmail.co.uk>
2019-01-02 15:02:33 +0000

    Update Friends table with various rows of data

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -13,8 +13,10 @@ connection = pymysql.connect(host = 'localhost',
                             db = 'Chinook')
 try:
     with connection.cursor() as cursor:
-        row = ("Bob", 28, "1990-02-06 23:04:56")
-        cursor.execute("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+        rows = [("Bob", 28, "1990-02-06 23:04:56"),
+                ("Jim", 56, "1955-05-09 11:12:45"),
+                ("Fred", 100, "1911-09-12 01:01:01")]
+        cursor.executemany("INSERT INTO Friends VALUES (%s, %s, %s);", rows)
         connection.commit()
 finally:
     # Close the connection, regardless of whether the code above was successful
